4a10a7b9be6f4e854c52d22988551e6a22dbdf36
jadjer <jadjer@ya.ru>
2022-08-26 21:42:35 +0300

    feat: First commit

---
+++ main/utils.hpp
@@ -0,0 +1,13 @@
+//
+// Created by jadjer on 24.08.22.
+//
+
+#pragma once
+
+float calcValueDivide256(int val);
+float calcValueMinus40(int val);
+float calcValueDivide10(int val);
+float calcValueDivide10(int val);
+float calcValueDivide16(int val);
+uint8_t calcChecksum(const uint8_t* data, uint8_t len);
+float calcKPHtoMPH(int val);
